a557ef0e1eb3f7a931e74572c1665644e744d3ce
jypLord <jygold2n@naver.com>
2026-05-04 15:25:03 +0900

    feat: Redis Streams 제거, Kafka 도입

--- src/main/java/com/jypLord/redis/streams/StreamEnvelope.java
+++
@@ -1,7 +0,0 @@
-package com.jypLord.redis.streams;
-
-import org.springframework.data.redis.connection.stream.MapRecord;
-
-public record StreamEnvelope(StreamKey streamKey, MapRecord<String, String, String> record) {
-
-}
